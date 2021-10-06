f0d67c964bb294dfab72f206428fd84ad90ca36f
Dominik Ehrenberg <dominik.ehrenberg@inovex.de>
2021-10-06 10:22:28 +0200

    Lösung Aufgabe 2 + Beschreibung Aufgabe 3

--- index.js
+++ index.js
@@ -7,7 +7,31 @@ document.addEventListener('DOMContentLoaded', async () => {
   // context.stroke();
 
   const background = new Background(context);
-  await background.init('assets/background1.png');
-  background.render(canvas.width, canvas.height);
+  const backgroundPromise = background.init('assets/background1.png');
+
+  const platforms = [
+    new Platform(context),
+    new Gap(context),
+    new Platform(context),
+    new Platform(context),
+    new Gap(context),
+    new Platform(context),
+    new Platform(context),
+    new Gap(context),
+    new Platform(context)
+  ];
+
+  const platformPromises = platforms
+    .map((platform, index) => {
+      platform.x = index * platform.width;
+      platform.y = 282;
+      return platform;
+    })
+    .map((platform) => platform.init());
+
+  Promise.all([backgroundPromise, ...platformPromises]).then(() => {
+    background.render(canvas.width, canvas.height);
+    platforms.forEach((platform) => platform.render());
+  });
     
 });
